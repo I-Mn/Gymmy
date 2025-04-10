fc04918150e1db1ec239da7a40a1002a4f505790
JiangHaru23 <muhammadridwan8873@gmail.com>
2025-04-10 12:10:55 +0700

    Edit Error

--- jadwal.cpp
+++ jadwal.cpp
@@ -3,8 +3,14 @@
 #include <ctime>
 using namespace std;
 
-
 struct JadwalLatihan {
+    string Bulan;
+    string NamaPeserta;
+    string DaftarLatihan[99]={};
+    struct DataLatihan;
+};
+
+struct DataLatihan {
     tm HariTanggal;
     string JenisLatihan;
     string Pelatih;
@@ -15,50 +21,50 @@ struct JadwalLatihan {
 };
 
 int main() {
-    JadwalLatihan jadwal;
+    DataLatihan latihan;
 
     cout << "Masukkan tahun: ";
-    cin >> jadwal.HariTanggal.tm_year;
-    jadwal.HariTanggal.tm_year -= 1900; 
+    cin >> latihan.HariTanggal.tm_year;
+    latihan.HariTanggal.tm_year -= 1900; 
 
     cout << "Masukkan bulan (1-12): ";
-    cin >> jadwal.HariTanggal.tm_mon;
-    jadwal.HariTanggal.tm_mon -= 1; 
+    cin >> latihan.HariTanggal.tm_mon;
+    latihan.HariTanggal.tm_mon -= 1; 
 
     cout << "Masukkan hari: ";
-    cin >> jadwal.HariTanggal.tm_mday;
+    cin >> latihan.HariTanggal.tm_mday;
 
-    cin.ignore(); // Clear the input buffer
+    cin.ignore(); 
     cout << "Masukkan jenis latihan: ";
-    getline(cin, jadwal.JenisLatihan);
+    getline(cin, latihan.JenisLatihan);
 
     cout << "Masukkan nama pelatih: ";
-    getline(cin, jadwal.Pelatih);
+    getline(cin, latihan.Pelatih);
 
     cout << "Masukkan jumlah anggota: ";
-    cin >> jadwal.jumlahAnggota;
+    cin >> latihan.jumlahAnggota;
 
     cout << "Masukkan durasi (dalam menit): ";
-    cin >> jadwal.durasi;
+    cin >> latihan.durasi;
 
     cin.ignore(); // Clear the input buffer
     cout << "Masukkan metode latihan: ";
-    getline(cin, jadwal.Metode);
+    getline(cin, latihan.Metode);
 
     cout << "Masukkan catatan pelatih: ";
-    getline(cin, jadwal.NotePelatih);
+    getline(cin, latihan.NotePelatih);
 
     // Output the data to verify
     cout << "\nData Jadwal Latihan:\n";
-    cout << "Tanggal: " << jadwal.HariTanggal.tm_mday << "/"
-         << jadwal.HariTanggal.tm_mon + 1 << "/"
-         << jadwal.HariTanggal.tm_year + 1900 << endl;
-    cout << "Jenis Latihan: " << jadwal.JenisLatihan << endl;
-    cout << "Pelatih: " << jadwal.Pelatih << endl;
-    cout << "Jumlah Anggota: " << jadwal.jumlahAnggota << endl;
-    cout << "Durasi: " << jadwal.durasi << " menit" << endl;
-    cout << "Metode: " << jadwal.Metode << endl;
-    cout << "Catatan Pelatih: " << jadwal.NotePelatih << endl;
+    cout << "Tanggal: " << latihan.HariTanggal.tm_mday << "/"
+         << latihan.HariTanggal.tm_mon + 1 << "/"
+         << latihan.HariTanggal.tm_year + 1900 << endl;
+    cout << "Jenis Latihan: " << latihan.JenisLatihan << endl;
+    cout << "Pelatih: " << latihan.Pelatih << endl;
+    cout << "Jumlah Anggota: " << latihan.jumlahAnggota << endl;
+    cout << "Durasi: " << latihan.durasi << " menit" << endl;
+    cout << "Metode: " << latihan.Metode << endl;
+    cout << "Catatan Pelatih: " << latihan.NotePelatih << endl;
 
     return 0;
 
